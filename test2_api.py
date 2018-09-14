a7d0c03db558daa3dcd6ed4f4b02650cbe429359
Simon Awiti <awitisimon23@gmail.com>
2018-09-14 10:24:33 +0300

    more tests

---
+++ test2_api.py
@@ -0,0 +1,29 @@
+
+import pytest
+from app import app
+
+@pytest.fixture
+def client(request):
+    test_client = app.test_client()
+
+    def teardown():
+        pass
+
+    request.addfinalizer(teardown)
+    return test_client
+
+def test_get_one_orders(client):
+    response = client.get('/api/v1/orders/<string:ids>')
+    assert response.status_code == 200
+
+def test_post_order(client):
+    response = client.post('api/v1/orders')
+    assert response.status_code == 201
+ 
+
+def test_update_orders(client):
+    response = client.put('/api/v1/orders/<string:ids>')
+    assert response.status_code == 202
+
+
+
